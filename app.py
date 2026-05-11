b7dbfe3e7e6768198d8ea840326a006337f509f8
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-05-11 13:01:01 +0100

    build: main layout and schemes

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('base.html')
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('base.html')
+    return render_template('contact.html')
 
 
 @app.route('/dashboard')
