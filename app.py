44272fbf935d4264ebd63eaaf5dce433be7c5318
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-05-14 09:26:20 +0100

    refactor: Remove unused pages from project and .py

--- app.py
+++ app.py
@@ -70,14 +70,6 @@ def home():
  return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 
 @app.route('/dashboard')
 @login_required
