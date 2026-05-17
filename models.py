85148dcad0d6028f64096b5ebe4fb81f95c288c2
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-05-17 12:34:33 +0100

    fix: error in model

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class User(UserMixin, db.Model):
     
 
     #===LINK USER TO Assignment===#
-    timetables_entries = db.relationship(
+    timetable_entries = db.relationship(
         "TimetableEntry",
         backref='user', 
         lazy=True,
