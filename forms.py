460f2330367da318e21a337569dc940f9496b152
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-05-19 12:09:56 +0100

    build: building wt forms to replace manual form handling - reg and login

--- forms.py
+++ forms.py
@@ -1,3 +1,62 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, SubmitField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+#====Registration and login ==#
+
+class RegistationForm(FlaskForm):
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(min=2, max=150),
+        ]
+    )
+
+    email = StringField(
+        "Email",
+        validators=[
+            DataRequired(),
+            Email(),
+            Length(max=150),
+        ]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=6),
+        ]
+    )
+
+    confirmpassword = PasswordField(
+        "Confirm_password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match.")
+        ]
+    )
+
+    submit = SubmitField("Create account")
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        "Email", 
+        validators=[
+            DataRequired(),
+            Email(),
+        ]
+    )
+
+    password = PasswordField(
+        "Password", 
+        validators=[
+            DataRequired(),
+            Email(),
+        ]
+    )
+
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Log in")
